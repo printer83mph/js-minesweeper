1d174d45e58d3a693d40bd86aace8ff2524f6d3a
Thomas Shaw <printer.83mph@gmail.com>
2018-05-10 19:01:36 -0400

    Add initial files

---
+++ main.js
@@ -0,0 +1,39 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+let mainWindow;
+
+function makeWindow() {
+  mainWindow = new BrowserWindow({width: 800, height: 800});
+
+  mainWindow.setMenu(null);
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file:",
+    slashes: true
+  }));
+
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", makeWindow);
+
+app.on("window-all-closed", function() {
+  if (process.platform != "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function() {
+  if (mainWindow === null) {
+    makeWindow();
+  }
+});
---
+++ script.js
@@ -0,0 +1,135 @@
+// dom stuff
+var butcustom, custominput;
+
+// mouse stuff
+var mouseX, mouseY, blockX, blockY;
+
+// grid stuff
+var canvas, ctx, size, bombs, visGrid, gridSize, blockSize;
+
+// bombs
+
+function drawGrid() {
+  ctx.fillStyle = "#d8d8d8";
+  ctx.fillRect(0, 0, size, size);
+
+  blockX = Math.floor(mouseX/blockSize);
+  blockY = Math.floor(mouseY/blockSize);
+
+  if (mouseX > 0 && mouseX < size && mouseY > 0 && mouseY < size) {
+    ctx.fillStyle ="#e0e0e0";
+    ctx.fillRect(Math.floor(mouseX/blockSize)*blockSize, Math.floor(mouseY/blockSize)*blockSize, blockSize, blockSize);
+  }
+
+  // draw divs
+  ctx.lineWidth = 1;
+  for (var i = 1; i < gridSize; i++) {
+    // draw col dividers
+    ctx.beginPath();
+    ctx.moveTo(i * blockSize, 0);
+    ctx.lineTo(i * blockSize, size);
+    // draw row dividers
+    ctx.moveTo(0, i * blockSize);
+    ctx.lineTo(size, i * blockSize);
+    ctx.stroke();
+  }
+
+  // window.requestAnimationFrame(drawGrid);
+}
+
+function bombsNear(x, y) {
+  var bc = 0;
+  for (var i = x-1; i < x + 2; i++) {
+    for (var j = y-1; j < y + 2; j++) {
+      for (var k in bombs) {
+        if ([i,j] == bombs[k]) {bc++}
+      }
+      // if ([i, j] in bombs) {
+      //   bc++;
+      // } else {console.log([i,j].toString() + " not in bombs")}
+    }
+  }
+  return bc;
+}
+
+function markSpot(x, y) {
+  drawGrid();
+}
+
+function uncoverSpot(x, y) {
+  if([x, y] in bombs) {
+    endGame(false);
+  }
+  drawGrid();
+}
+
+function makeGrid(gSize, fill) {
+  // output grid
+  out = [];
+  // add all the zeroes
+  for (var i = 0; i < gridSize; i++) {
+    var thisRow = [];
+    for (var j = 0; j < gridSize; j++) {
+      thisRow.push(fill);
+    }
+    out.push(thisRow);
+  }
+
+  return out;
+}
+
+function resizeGrid(gSize) {
+  // refresh sizes
+  gridSize = gSize;
+  blockSize = size/gridSize;
+  // make visGrid
+  visGrid = makeGrid(gSize, 0);
+  // re-place bombs
+  bombs = [];
+  var oldJawn, newJawn;
+  for(var i = 0; i < 10; i++) {
+    do {
+      newJawn = [Math.floor(Math.random()*gridSize), Math.floor(Math.random()*gridSize)];
+    } while (newJawn in bombs);
+    bombs.push(newJawn);
+  }
+  drawGrid();
+}
+
+window.onload = function() {
+  butcustom = document.getElementById("custom");
+  custominput = document.getElementById("custominput");
+
+  // game stuff
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  size = canvas.width;
+  resizeGrid(8);
+
+  // canvas stuff
+  ctx.strokeStyle = "#ffffff"
+
+  // hover stuff
+  document.addEventListener("mousemove", function(e) {
+    mouseX = e.clientX - canvas.offsetLeft;
+    mouseY = e.clientY - canvas.offsetTop;
+    drawGrid();
+  });
+
+  // click stuff
+  document.addEventListener("contextmenu", function(e) {
+    if (mouseX > 0 && mouseX < size && mouseY > 0 && mouseY < size) {
+      e.preventDefault();
+      markSpot(blockX, blockY);
+    }
+  });
+
+  document.addEventListener("click", function(e) {
+    if (mouseX > 0 && mouseX < size && mouseY > 0 && mouseY < size) {
+      e.preventDefault();
+      uncoverSpot(blockX, blockY);
+    }
+  });
+
+  drawGrid();
+}
